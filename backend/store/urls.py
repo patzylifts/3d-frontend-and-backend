c9937b966b8a9d5a8dc1d03cdd346278a25d347f
Nigler10 <cchrisjoven10@gmail.com>
2026-03-02 16:07:52 +0800

    zenshin zenrei MORE MORE JUMP!

--- backend/store/urls.py
+++ backend/store/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Products
     path('products/', views.get_products, name='product_list'),
+    path('products/<int:pk>/', views.get_product),
+    
+    #Categories
     path('categories/', views.get_categories, name='category_list'),
 ]
